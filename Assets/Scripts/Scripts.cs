1f21fe3178d0e4b4af8fe6ed1fd57805dd5da338
raining-cat <159646115+raining-cat@users.noreply.github.com>
2024-02-22 23:53:56 +0500

    1exersize

---
+++ Assets/Scripts/Scripts.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts: MonoBehaviour
+{
+    public Transform BlueCube1;
+    public Transform BlueCube2;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(BlueCube1.position, BlueCube2.position, 0.5f);
+    }
+}
